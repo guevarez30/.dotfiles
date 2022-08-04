f17720ff7f4896879348f966998dfdfc3c6aa170
Taylor Guevarez <guevarez30@gmail.com>
2022-08-04 08:52:07 -0400

    IMPLEMENTS rust autoformat

--- nvim/.config/nvim/lua/guevarez/maps.lua
+++ nvim/.config/nvim/lua/guevarez/maps.lua
@@ -8,8 +8,8 @@ function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-map("n", "<Leader>v", ":Vexplore<CR>")
-map("n", "<Leader>e", ":Explore<CR>")
+map("n", "<Leader>s", ":Vexplore<CR>")
+map("n", "<Leader>S", ":Hexplore<CR>")
 map("n", "<Leader>w", "<C-w>w")
 map("n", "<Leader>o", "o<Esc>")
 map("n", "<Leader>O", "O<Esc>")
@@ -20,9 +20,14 @@ map("n", "<C-Down>", "ddp")
 
 -- Telescope
 map("n", "<C-p>", ":Telescope find_files <CR>")
+map("n", "<C-f>", ":Telescope live_grep <CR>")
 
 -- Harpoon
 map("n", "<leader>m", ":lua require('harpoon.mark').add_file() <CR>")
 map("n","<leader>ml", ":lua require('harpoon.ui').toggle_quick_menu() <CR>")
 map("n","<leader>mn", ":lua require('harpoon.ui').nav_next() <CR>")
 map("n","<leader>mp", ":lua require('harpoon.ui').nav_prev() <CR>")
+
+-- Line Movement
+map("n", "<leader>h", "0")
+map("n", "<leader>l", "$")
