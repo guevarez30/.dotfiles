a83c5e5fd6dd3a32dac9977ded1fcc1d98662ba4
Taylor Guevarez <guevarez30@gmail.com>
2022-11-08 18:07:13 -0500

    REMOVES hints

--- nvim/.config/nvim/lua/guevarez/init.lua
+++ nvim/.config/nvim/lua/guevarez/init.lua
@@ -15,4 +15,12 @@ require("guevarez.lspconfig")
 require("guevarez.treesitter")
 
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = false,
+    virtual_text = true,
+    signs = false,
+    update_in_insert = false,
+  }
+)
 
