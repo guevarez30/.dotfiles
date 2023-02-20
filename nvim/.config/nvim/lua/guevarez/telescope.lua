5d41e24d413b826fed816b227d2705fec862bea1
Taylor Guevarez <taylor@aerialapplications.com>
2023-02-19 22:37:14 -0500

    file preview on telescope

--- nvim/.config/nvim/lua/guevarez/telescope.lua
+++ nvim/.config/nvim/lua/guevarez/telescope.lua
@@ -1,6 +1,8 @@
 local telescope = require("telescope")
 telescope.setup{  
   defaults = { 
+	  previewer = true,
+	  file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
     file_ignore_patterns = { 
       "node_modules", "storage" 
     }
