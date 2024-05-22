67028227434e6a2b2da0aa5202861d9ee5fc9e9e
Taylor Guevarez <taylor.guevarez@shift4.com>
2024-05-22 12:42:32 -0400

    Autoresizes windows on splits

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,3 +28,8 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 		require("conform").format({ bufnr = args.buf })
 	end,
 })
+
+vim.api.nvim_create_autocmd("WinResized", {
+	pattern = "*",
+	command = "wincmd =",
+})
